373a116d84fcba2c6759c06861a9fd3e9252bcff
Samiksha099 <83509192+Samiksha099@users.noreply.github.com>
2021-05-07 19:34:03 +0530

    Add files via upload

---
+++ Lemon.java
@@ -0,0 +1,11 @@
+public class Lemon{
+public static void main(String args[])
+{
+long l =22;
+int e = (int) l + 3;
+boolean m= l < e ;
+float o =12.5f;
+double n = o * 2;
+System.out.println(l+ "e" + m + "o" +n);
+}
+}
