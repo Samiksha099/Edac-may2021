aabccca72338f98e35b2bae47f38ad7e4ed0092f
Samiksha099 <83509192+Samiksha099@users.noreply.github.com>
2021-06-16 22:12:23 +0530

    Add files via upload

---
+++ AssignmentTwo.java
@@ -0,0 +1,263 @@
+  // PATTERN 1//
+/*
+public class AssignmentTwo{
+public static void main(String args[])
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print(j+" ");
+}
+System.out.println();
+}
+}
+}
+*/
+/*
+output:
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+*/
+
+  
+   // PATTERN 2 //
+/*
+public class AssignmentTwo{
+public static void main(String args[])
+{
+int alphabet=65;
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print((char)(alphabet++)+" ");
+}
+alphabet=65;
+System.out.println();
+}
+}
+}
+*/
+
+/*
+output:
+
+A
+A B
+A B C
+A B C D
+A B C D E
+*/
+
+/*
+  // PATTERN 3//
+public class AssignmentTwo{
+public static void main(String args[])
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print("*"+" ");
+}
+System.out.println();
+}
+}
+}
+*/
+/*
+
+OUTPUT:
+*
+* *
+* * *
+* * * *
+* * * * *
+
+*/
+
+   // PATTERN 4//
+/*
+public class AssignmentTwo{
+public static void main(String args[])
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print(i+" ");
+}
+System.out.println();
+}
+}
+}
+*/
+/*
+Output:
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+*/
+
+
+   //PATTERN 5//
+/*
+public class AssignmentTwo{
+public static void main(String args[])
+{
+int alphabet=65;
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print((char)(alphabet)+" ");
+}
+alphabet++;
+System.out.println();
+}
+}
+}
+*/
+
+/*
+Output:
+A
+B B
+C C C
+D D D D
+E E E E E
+*/
+   
+
+
+
+
+   //PATTERN 11//
+/*
+public class AssignmentTwo
+{
+public static void main(String args[])
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=5;j>=i;j--)
+{
+System.out.print(" ");
+}
+for(int k=1;k<=i;k++)
+{
+System.out.print(" *");
+}
+System.out.println();
+}
+}
+}
+*/
+
+/*
+Output:
+
+      *
+     * *
+    * * *
+   * * * *
+  * * * * *
+
+*/
+    
+
+
+
+
+   //PATTERN 14//
+ /*
+public class AssignmentTwo{
+public static void main(String args[])
+{
+ for(int i=5;i>=1;i--)
+{
+ for(int j=1;j<=i;j++)
+{ 
+System.out.print(" "+j);
+ }
+System.out.println();
+}
+}
+}*/
+
+/*
+Output:
+ 1 2 3 4 5
+ 1 2 3 4
+ 1 2 3
+ 1 2
+ 1
+*/
+
+
+ //PATTERN 15//
+/*
+public class AssignmentTwo{
+public static void main(String args[])
+{
+ for(int i=1;i<=5;i++)
+{
+ for(int j=5;j>=i;j--)
+{ 
+System.out.print(" "+j);
+ }
+System.out.println();
+}
+}
+}*/
+
+/*
+Output:
+
+   5 4 3 2 1
+   5 4 3 2
+   5 4 3
+   5 4
+   5
+*/
+
+
+  // PATTERN 19//
+/*
+public class AssignmentTwo
+{
+public static void main(String args[])
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=5;j>=i;j--)
+{
+System.out.print(" ");
+}
+for(int k=1;k<=i;k++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}*/
+
+/*
+Output:
+     *
+    **
+   ***
+  ****
+ *****
+
+*/
+
+
+ 
